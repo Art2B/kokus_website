77f10f96a1e5ec80058441666a203164e1ef000c
Arthur Battut <arthur@soixantecircuits.fr>
2015-06-25 14:04:13 +0200

    work on site

--- app/scripts/controllers/demo.js
+++ app/scripts/controllers/demo.js
@@ -10,6 +10,7 @@
 angular.module('kokusSiteApp')
 .controller('DemoCtrl', function ($scope) {
   $scope.instance = new Kokus({idContainer: "demo"});
+  console.log($scope.instance);
 
   $scope.addTree = function(){
     new Kokus.Tree({x: Helpers.getRandomInt(0,360), y: Helpers.getRandomInt(0,360), z: Helpers.getRandomInt(0,360)}, {}, $scope.instance);
