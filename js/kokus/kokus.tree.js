502cd65337116a839a2b94f44c98a9e1f17f2b3b
Rouksana <rouksana.shannon@gmail.com>
2015-06-25 13:40:31 +0200

    fix house & tree

--- js/kokus/kokus.tree.js
+++ js/kokus/kokus.tree.js
@@ -36,13 +36,14 @@ Kokus.Tree.prototype = {
     var geometryBase = new THREE.CylinderGeometry( _self.options.leafBaseSize/4, _self.options.leafBaseSize/4, _self.options.leafBaseHeight*0.15, 4);       
     var pineLeaf = new THREE.Mesh( geometryLeaf, materialLeaf);
     var pineBase = new THREE.Mesh( geometryBase, materialBase);
+    var space = _self.options.leafBaseSize / 10;
 
     pineLeaf.applyMatrix( new THREE.Matrix4().makeTranslation(0, (_self.options.leafBaseHeight/2)+((_self.options.leafBaseHeight*0.15)/2), 0) );
     pineBase.applyMatrix( new THREE.Matrix4().makeTranslation(0, (_self.options.leafBaseHeight*0.15)/2, 0) );
 
     _self.tree = new THREE.Mesh();
     _self.tree.add(pineLeaf).add(pineBase);
-    _self.tree.position.y = _self.kokusObject.world.planet.geometry.parameters.radius;
+    _self.tree.position.y = _self.kokusObject.world.planet.geometry.parameters.radius - space;
 
     _self.pivot = new THREE.Object3D();
     _self.pivot.add(_self.tree);
