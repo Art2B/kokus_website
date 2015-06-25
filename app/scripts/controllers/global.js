57a24cdefb504de9cf03935f0baf7ae8aeba3609
Arthur Battut <arthur@soixantecircuits.fr>
2015-06-25 21:55:54 +0200

    add header

--- app/scripts/controllers/global.js
+++ app/scripts/controllers/global.js
@@ -10,8 +10,14 @@
 angular.module('kokusSiteApp')
 .controller('GlobalCtrl', function ($scope, $location) {
   $scope.year = new Date().getFullYear();
+  $scope.showHeader = false;
 
   $scope.$on('$routeChangeStart', function(next, current) { 
     $scope.bodyClass = $location.path().substr(1);
+    if($location.path().substr(1) != ''){
+      $scope.showHeader = true;
+    } else {
+      $scope.showHeader = false;
+    }
   });
 });
